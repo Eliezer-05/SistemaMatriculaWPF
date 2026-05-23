8339f11888c0210e060e9f4153a659021cdd4ca2
Eliezer <sist.erivas@uml.edu.ni>
2026-05-23 12:44:38 -0600

    Proyecto inicial WPF

---
+++ views/Matriculas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaMatriculas.views
+{
+    /// <summary>
+    /// Lógica de interacción para Matriculas.xaml
+    /// </summary>
+    public partial class Matriculas : Window
+    {
+        public Matriculas()
+        {
+            InitializeComponent();
+        }
+    }
+}
